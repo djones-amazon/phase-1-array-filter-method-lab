234c5573d13fe00f840b40a1b8db47f00f4161b8
Devin Jones <devin.jones21083@gmail.com>
2023-04-02 20:10:31 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const findMatching = function (driverList, filterValue) {
+    let matchingRecords = driverList.filter(driver => driver.toUpperCase() === filterValue.toUpperCase());
+    return matchingRecords;
+}
+
+const fuzzyMatch = function (driverList , query) {
+    let matchingRecords = driverList.filter(driver => driver.at(0) === query.at(0));
+    return matchingRecords;
+}
+
+const matchName = function (driverList, query) {
+    let matchingRecords = driverList.filter(driver => driver.name === query);
+    return matchingRecords;
+}
